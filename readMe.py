e1f2b72f35f8b2115dafbb4607f56548c3ed8389
liangliangyuese <929783514@qq.com>
2019-07-02 14:55:22 +0800

    项目基础框架

---
+++ readMe.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# 用户 user
+# 文章 article
+# 评论 comment
+# 首页 home
+
+# -------
+# Oauth登陆
+# 全文检索
+# Memcache缓存
+# seo
+# 图床
+# django-compressor
+# 邮件提醒
+# 微信公众号
+#
+# pip install --upgrade mysqlclient
