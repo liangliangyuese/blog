ab492c1f767995a4f1b7bcedd363531d2bd1b134
liangliangyuese <929783514@qq.com>
2019-07-05 13:35:01 +0800

    用户模型

---
+++ blog/user/forms.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.Form):
+    pass
+
+
+class LoginForm(forms.Form):
+    pass
+
+
+class RestForm(forms.Form):
+    pass
