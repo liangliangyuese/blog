b3299a1747d98acc5634f1eeee263e7658d1bfbb
liangliangyuese <929783514@qq.com>
2019-08-21 17:11:02 +0800

    continue learning

--- blog/static/js/introduce.js
+++ blog/static/js/introduce.js
@@ -8,3 +8,26 @@ myimg.onclick = function () {
         myimg.setAttribute('src', '/static/image/791e1a3aff021817b4028b1d91b92f80.jpeg')
     }
 };
+
+//替换h1标签内容
+function setHead(name) {
+    let myHead = document.querySelector('h1');
+    myHead.textContent = '欢迎你' + name + '!'
+}
+
+//设置输入 内容设置到storage
+function setName() {
+    let myName = prompt('输入名称');
+    localStorage.setItem('name', myName);
+    setHead(myName)
+}
+
+let storgname = localStorage.getItem('name');
+if (storgname) {
+    setHead(storgname)
+} else {
+    setName();
+}
+
+let mybutton = document.querySelector('button');
+mybutton.onclick = setName;
